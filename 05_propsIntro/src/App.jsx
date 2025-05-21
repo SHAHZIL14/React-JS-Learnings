4f663744927f395a395f7489ce4de090cb9cee84
Shazil Raza <shazilraza8@gmail.com>
2025-05-21 15:56:26 +0530

    Props Introduction

---
+++ 05_propsIntro/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './Card'
+import './App.css'
+
+function App() {
+  let propObject={
+    h5:"Noteworthy technology acquisitions 2021",
+    p:"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+    Isrc:"https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+  }
+  return <>
+  <Card prop1 = {propObject}/>
+  </>
+}
+
+export default App
